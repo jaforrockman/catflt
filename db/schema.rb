b8ff29a56565abbe6253c95b19ee5a7ebc97b562
jaforrockman <jaforrockman695@gmail.com>
2023-01-07 13:22:45 +0600

    added devise for user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230107071350) do
+ActiveRecord::Schema.define(version: 20230107072109) do
 
   create_table "caterings", force: :cascade do |t|
     t.string "name"
@@ -60,6 +60,13 @@ ActiveRecord::Schema.define(version: 20230107071350) do
     t.integer "catering_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vouchers", force: :cascade do |t|
